softmax means ReLU for inference and fix auto-rescaling

sinabs/layers/iaf_conv2d.py
@@ -305,7 +305,7 @@ def from_conv2d_keras_conf(
             torch_analogue_layerActivation = nn.Sigmoid()
             layer_list.append((layer_nameActivation, torch_analogue_layerActivation))
         elif layer_config["config"]["activation"] == "softmax":
-            torch_analogue_layerActivation = nn.Softmax2d()
+            torch_analogue_layerActivation = nn.ReLU()
             layer_list.append((layer_nameActivation, torch_analogue_layerActivation))
         else:
             raise NotImplementedError
@@ -397,7 +397,7 @@ def from_dense_keras_conf(
             torch_analogue_layerActivation = nn.Sigmoid()
             layer_list.append((layer_nameActivation, torch_analogue_layerActivation))
         elif layer_config["config"]["activation"] == "softmax":
-            torch_analogue_layerActivation = nn.Softmax2d()
+            torch_analogue_layerActivation = nn.ReLU()
             layer_list.append((layer_nameActivation, torch_analogue_layerActivation))
         else:
             raise NotImplementedError

sinabs/utils.py
@@ -86,6 +86,7 @@ def get_activations(torchanalog_model, tsrData, name_list=None):
 
     # Do a forward pass
     with torch.no_grad():
+        torchanalog_model.eval()
         torchanalog_model(tsrData)
 
     # Remove hooks
@@ -125,7 +126,6 @@ def get_network_activations(
     if bRate:
         spike_counts = [(counts / tSim * 1000) for counts in spike_counts]
     return spike_counts
-
 
 
 def summary(model: TorchLayer) -> pd.DataFrame: