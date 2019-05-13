99f5e12c6cf814bf7391c0e457cbf43717ac54d4
Sadique Sheik <sadique.sheik@aictx.ai>
2019-05-13 14:58:32 +0200

    fixed version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
     name="sinabs",
-    version="0.1dev",
+    version="0.1.0",
     packages=["sinabs"],
     license="All rights reserved aiCTX AG",
     install_requires=['numpy', 'pandas', 'tensorflow', 'torch'],
