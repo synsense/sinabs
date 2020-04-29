f41e7e7b004f83200cf83fd9502bc8bf516392df
Sadique Sheik <sadique.sheik@aictx.ai>
2020-04-29 09:43:30 +0200

    model build method separately added

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -2,10 +2,8 @@ import torch
 import torch.nn as nn
 
 
-def test_sinabs_model_to_onnx():
-    import onnx
+def build_model():
     from sinabs.from_torch import from_model
-    from sinabs.onnx import print_onnx_model, get_graph, print_graph
 
     layers = [
         nn.Conv2d(2, 8, kernel_size=2),
@@ -17,28 +15,34 @@ def test_sinabs_model_to_onnx():
     ]
 
     ann = nn.Sequential(*layers)
-    snn = from_model(ann)
-    print(snn)
+    net = from_model(ann)
+    return net
+
+
+def test_sinabs_model_to_onnx():
+    import onnx
+    from sinabs.onnx import print_onnx_model, get_graph, print_graph
+
+    net = build_model()
     dummy_input = torch.zeros([1, 2, 64, 64])  # One time step
-    snn(dummy_input) # first pass to create all state variables
+    net(dummy_input)  # first pass to create all state variables
     fname = "snn.onnx"
 
     torch.onnx.export(
-        snn.spiking_model,
+        net.spiking_model,
         dummy_input,
         fname,
         input_names=["tchw_input"],
         output_names=["tchw_output"],
-        dynamic_axes={"tchw_input": [0],"tchw_output": {0: "time"}},
+        dynamic_axes={"tchw_input": [0], "tchw_output": {0: "time"}},
         verbose=False,
     )
     snn_model = onnx.load(fname)
-    ann_graph = get_graph(snn, dummy_input)
+    ann_graph = get_graph(net, dummy_input)
     print("................")
     print_graph(ann_graph)
     print("................")
     print_onnx_model(snn_model)
-
 
 
 def test_graph_generation_ann():
@@ -54,4 +58,5 @@ def test_graph_generation_ann():
     onnx_model = onnx.load(fname)
 
     print_onnx_model(onnx_model)
+
 
