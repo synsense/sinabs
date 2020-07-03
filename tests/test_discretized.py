26831ab193b8f8ab0707ad021b0289592ffa8ca4
Felix Bauer <felix.bauer@aictx.ai>
2020-07-03 09:50:27 +0200

    Smaller fixes in discretize

--- tests/test_discretized.py
+++ tests/test_discretized.py
@@ -1,6 +1,7 @@
-from sinabs.backend.speck import discretize
+from copy import deepcopy
 import numpy as np
 import torch
+from sinabs.backend.speck import discretize
 from sinabs.layers import SpikingLayerBPTT
 
 # - Test tensor to be discretized
@@ -19,38 +20,235 @@ float_tensor = torch.tensor(
     ]
 )
 
+MIN_STATE = -(2 ** (discretize.SPECK_STATE_PRECISION_BITS - 1))
+MAX_STATE = -MIN_STATE - 1
+MIN_WEIGHT = -(2 ** (discretize.SPECK_WEIGHT_PRECISION_BITS - 1))
+MAX_WEIGHT = -MIN_WEIGHT - 1
 ## -- Layers to be discretized
 
 torch.manual_seed(8)
 # - Convolutional
 weight = torch.randn(4, 2, 3, 3) * 10
 bias = torch.randn(4) * 10
-conv_lyr = torch.nn.Conv2d(in_channels=2, out_channels=4, kernel_size=(3, 3))
+conv_lyr = torch.nn.Conv2d(
+    in_channels=2, out_channels=4, kernel_size=(3, 3), bias=False
+)
 conv_lyr.weight = torch.nn.Parameter(weight)
-conv_lyr.bias = torch.nn.Parameter(bias)
 
 # - Spiking layer
+np.random.seed(4)
 thr = np.random.random() * 10
 thr_low = -np.random.random() * 10
 spk_lyr = SpikingLayerBPTT(threshold=thr, threshold_low=thr_low, membrane_subtract=True)
 
-# - Evolve to initialize layer state
-inp = torch.randn(30, 2, 32, 32)
-out_conv = conv_lyr(inp)
-out_spk = spk_lyr(inp)
+
+def validate_common_scaling(conv_lyr, spk_lyr, weight, bias, thr, thr_low, state):
+
+    if thr is not None:
+        # Guess scaling based on threshold
+        scale = thr / spk_lyr.threshold
+        reference = spk_lyr.threshold
+
+        thrs = torch.tensor((thr, thr_low))
+        thrs_old = torch.tensor((spk_lyr.threshold, spk_lyr.threshold_low))
+    else:
+        scale = torch.max(weight) / torch.max(conv_lyr.weight)
+        reference = torch.max(weight)
+        thrs = thrs_old = None
+
+    # Error should be within what is expected from rounding
+    for orig, new in zip(
+        (conv_lyr.weight, conv_lyr.bias, spk_lyr.state, thrs_old),
+        (weight, bias, state, thrs),
+    ):
+        if new is not None:
+            tol = 0.5 * (1 + torch.abs(new / reference))
+            assert (torch.abs(scale * orig - new) <= tol).all()
+    # Make sure that new values are in allowed range
+    for new in (weight, bias):
+        if new is not None:
+            assert (new <= MAX_WEIGHT).all()
+            assert (new >= MIN_WEIGHT).all()
+    for new in (thrs, state):
+        if new is not None:
+            assert (state <= MAX_STATE).all()
+            assert (state >= MIN_STATE).all()
 
 
-def validate_common_scaling(conv_lyr, spk_lyr, weight, bias, thr, thr_low, state=None):
-    weight_scalings = weight / conv_lyr.weight
-    if bias is not None:
-        bias_scaling = bias / conv_lyr.bias
-    thr_scaling = thr / spk_lyr.threshold
-    thr_low_scaling = thr_low / spk_lyr.threshold_low
+def validate_discretization(conv_lyr, spk_lyr, inplace=False, to_int=True):
+    if inplace:
+        conv_discr, spk_discr = discretize.discretize_conv_spike_(
+            conv_lyr, spk_lyr, to_int=to_int
+        )
+    else:
+        conv_discr, spk_discr = discretize.discretize_conv_spike(
+            conv_lyr, spk_lyr, to_int=to_int
+        )
+    validate_common_scaling(
+        conv_lyr,
+        spk_lyr,
+        conv_discr.weight,
+        conv_discr.bias,
+        spk_discr.threshold,
+        spk_discr.threshold_low,
+        spk_discr.state,
+    )
+    return conv_discr, spk_discr
 
 
 def test_discretize_conv_spike():
+    # - Make copies of original layers
+    conv_copy = deepcopy(conv_lyr)
+    spk_copy = deepcopy(spk_lyr)
+
     # - Discretization of convolutional and spiking layer
-    conv_discr, spk_discr = discretize.discretize_conv_spike(conv_lyr, spk_lyr)
+    validate_discretization(conv_copy, spk_copy)
+
+    # - Set bias
+    conv_lyr.bias = torch.nn.Parameter(bias)
+    conv_copy.bias = torch.nn.Parameter(bias)
+    validate_discretization(conv_copy, spk_copy)
+
+    # - Evolve to initialize layer state
+    inp = torch.randn(30, 2, 32, 32)
+    out_conv = conv_copy(inp)
+    spk_copy(out_conv)
+    spk_lyr(out_conv)
+    conv_discr, spk_discr = validate_discretization(conv_copy, spk_copy)
+
+    # Make sure that discretization did not happen in-place
+    assert (conv_lyr.weight == conv_copy.weight).all()
+    assert (conv_lyr.bias == conv_copy.bias).all()
+    assert (spk_lyr.state == spk_copy.state).all()
+    assert spk_lyr.threshold == spk_copy.threshold
+    assert spk_lyr.threshold_low == spk_copy.threshold_low
+
+    # Make sure that elements are integers
+    for obj in (conv_discr.weight, conv_discr.bias, spk_discr.state):
+        assert obj.dtype == torch.int
+    for obj in (spk_discr.threshold, spk_discr.threshold):
+        assert isinstance(obj, (int, np.integer))
+
+    # - In-place mutations
+    conv_discr, spk_discr = validate_discretization(conv_copy, spk_copy, inplace=True)
+
+    # Make sure that discretization did happen in-place
+    assert (conv_discr.weight == conv_copy.weight).all()
+    assert (conv_discr.bias == conv_copy.bias).all()
+    assert (spk_discr.state == spk_copy.state).all()
+    assert spk_discr.threshold == spk_copy.threshold
+    assert spk_discr.threshold_low == spk_copy.threshold_low
+
+    # - No conversion to integers
+    conv_copy = deepcopy(conv_lyr)
+    spk_copy = deepcopy(spk_lyr)
+
+    conv_discr, spk_discr = validate_discretization(conv_copy, spk_copy, to_int=False)
+
+    # Make sure that discretization did not happen in-place
+    assert (conv_lyr.weight == conv_copy.weight).all()
+    assert (conv_lyr.bias == conv_copy.bias).all()
+    assert (spk_lyr.state == spk_copy.state).all()
+    assert spk_lyr.threshold == spk_copy.threshold
+    assert spk_lyr.threshold_low == spk_copy.threshold_low
+
+    # Make sure that elements are floats
+    for obj in (conv_discr.weight, conv_discr.bias, spk_discr.state):
+        assert obj.dtype == torch.float
+
+
+def test_discr_conv():
+    # - Add biases and evolve to initialize layer state
+    conv_lyr.bias = torch.nn.Parameter(bias)
+
+    conv_copy = deepcopy(conv_lyr)
+    spk_copy = deepcopy(spk_lyr)
+
+    inp = torch.randn(30, 2, 32, 32)
+    out_conv = conv_copy(inp)
+    spk_copy(out_conv)
+    spk_lyr(out_conv)
+
+    conv_discr = discretize.discretize_conv(
+        conv_copy, spk_copy.threshold, spk_copy.threshold_low, spk_copy.state
+    )
+    validate_common_scaling(
+        conv_copy, spk_copy, conv_discr.weight, conv_discr.bias, None, None, None,
+    )
+
+    # Make sure that discretization did not happen in-place
+    assert (conv_lyr.weight == conv_copy.weight).all()
+    assert (conv_lyr.bias == conv_copy.bias).all()
+
+    # - In-place
+    conv_discr = discretize.discretize_conv_(
+        conv_copy, spk_copy.threshold, spk_copy.threshold_low, spk_copy.state
+    )
+    validate_common_scaling(
+        conv_copy, spk_copy, conv_discr.weight, conv_discr.bias, None, None, None,
+    )
+
+    # Make sure that discretization did happen in-place
+    assert (conv_discr.weight == conv_copy.weight).all()
+    assert (conv_discr.bias == conv_copy.bias).all()
+
+    # - Make sure that spike layer elements did not get mutated
+    assert (spk_lyr.state == spk_copy.state).all()
+    assert spk_lyr.threshold == spk_copy.threshold
+    assert spk_lyr.threshold_low == spk_copy.threshold_low
+
+
+def test_discr_spk():
+    # - Add biases and evolve to initialize layer state
+    conv_lyr.bias = torch.nn.Parameter(bias)
+
+    conv_copy = deepcopy(conv_lyr)
+    spk_copy = deepcopy(spk_lyr)
+
+    inp = torch.randn(30, 2, 32, 32)
+    out_conv = conv_copy(inp)
+    spk_copy(out_conv)
+    spk_lyr(out_conv)
+
+    conv_copy = deepcopy(conv_lyr)
+    spk_copy = deepcopy(spk_lyr)
+
+    spk_discr = discretize.discretize_spk(spk_copy, conv_copy.weight, conv_copy.bias)
+    validate_common_scaling(
+        conv_copy,
+        spk_copy,
+        None,
+        None,
+        spk_discr.threshold,
+        spk_discr.threshold_low,
+        spk_discr.state,
+    )
+
+    # Make sure that discretization did not happen in-place
+    assert (spk_lyr.state == spk_copy.state).all()
+    assert spk_lyr.threshold == spk_copy.threshold
+    assert spk_lyr.threshold_low == spk_copy.threshold_low
+
+    # - In-place
+    spk_discr = discretize.discretize_spk_(spk_copy, conv_copy.weight, conv_copy.bias)
+    validate_common_scaling(
+        conv_copy,
+        spk_copy,
+        None,
+        None,
+        spk_discr.threshold,
+        spk_discr.threshold_low,
+        spk_discr.state,
+    )
+
+    # Make sure that discretization did happen in-place
+    assert (spk_discr.state == spk_copy.state).all()
+    assert spk_discr.threshold == spk_copy.threshold
+    assert spk_discr.threshold_low == spk_copy.threshold_low
+
+    # - Make sure that conv layer elements did not get mutated
+    assert (conv_lyr.weight == conv_copy.weight).all()
+    assert (conv_lyr.bias == conv_copy.bias).all()
 
 
 def test_discr_tensor():
@@ -58,12 +256,14 @@ def test_discr_tensor():
 
     discr_tensor = discretize.discretize_tensor(float_tensor, scaling, to_int=False)
 
-    assert torch.isclose(discr_tensor, float_tensor * scaling)
+    assert torch.isclose(discr_tensor, float_tensor * scaling, atol=0.5).all()
     assert discr_tensor.dtype == torch.float
 
     discr_tensor_int = discretize.discretize_tensor(float_tensor, scaling, to_int=True)
 
-    assert torch.isclose(discr_tensor_int, float_tensor * scaling)
+    assert torch.isclose(
+        discr_tensor_int.float(), float_tensor * scaling, atol=0.5
+    ).all()
     assert discr_tensor_int.dtype == torch.int
 
 
