0d5fe4be92c0a80e5c78e5ef5363f133d2ccf2fa
sadique.sheik <sadique.sheik@synsense.ai>
2020-11-25 19:28:54 +0100

    pbr based project versioning and gitlab ci added

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from distutils.core import setup
 
 setup(
-    name="sinabs-dynapcnn",
-    version="0.0.1dev",
-    packages=["sinabs.backend.dynapcnn"],
-    license="All rights reserved aiCTX AG",
-    install_requires=["sinabs", "samna>=0.3"],
+    setup_requires=['pbr'],
+    pbr=True
 )
