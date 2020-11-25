7e13285f395eadfd357c3743aa5c2ce254fa1e33
sadique.sheik <sadique.sheik@synsense.ai>
2020-11-25 17:45:06 +0100

    Samna requirement updated

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.0.1dev",
     packages=["sinabs.backend.dynapcnn"],
     license="All rights reserved aiCTX AG",
-    install_requires=["sinabs", "samna"],
+    install_requires=["sinabs", "samna>=0.3"],
 )
