f842e1d5d420e19e81f0a466dd5eb1b17ec8b146
Felix Bauer <felix.bauer@aictx.ai>
2021-06-25 11:49:07 +0200

    Remove Squeeze/Unsqueeze helper classes

--- sinabs/layers/pack_dims.py
+++ sinabs/layers/pack_dims.py
@@ -13,30 +13,6 @@ class Squeeze(ABC):
     pass
 
 
-class SqueezeBatchTime(nn.Module):
-    """
-    Convenience layer that
-    """
-
-    def __init__(self, batch_size=1):
-        super().__init__()
-        self.batch_size = batch_size
-
-    def forward(self, data):
-        all_spikes = data.reshape((-1, *data.shape[2:]))
-        return all_spikes
-
-
-class UnsqueezeBatchTime(nn.Module):
-    def __init__(self, batch_size=1):
-        super().__init__()
-        self.batch_size = batch_size
-
-    def forward(self, data):
-        syn_out = data.reshape((self.batch_size, -1, *data.shape[1:]))
-        return syn_out
-
-
 def squeeze_class(cls: Type) -> Type:
     """
     Factory function to generate a subclass of a given class, whose forward
