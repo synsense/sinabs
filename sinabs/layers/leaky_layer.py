4d01a0059e5e7cb069072fb36131ad31e33443ce
sadique.sheik <sadique.sheik@synsense.ai>
2021-08-25 09:35:13 +0200

    skeleton code

---
+++ sinabs/layers/leaky_layer.py
@@ -0,0 +1,104 @@
+from typing import Tuple, Union
+import torch
+from .pack_dims import squeeze_class
+from .functional import threshold_subtract, threshold_reset
+
+
+__all__ = ["ExpLeak", "ExpLeakSqueeze"]
+
+# Learning window for surrogate gradient
+window = 1.0
+
+
+class ExpLeak(torch.nn.Module):
+    def __init__(
+        self,
+        tau_mem: Union[float, torch.Tensor],
+    ):
+        """
+        Pytorch implementation of a exponential leaky layer, that is equivalent to a exponential synapse or a low-pass filter.
+
+        Parameters
+        ----------
+        tau_leak: float
+            Rate of leak of the state
+        """
+        super().__init__()
+        self.register_buffer("state", torch.zeros(1))
+        self.tau_mem = tau_mem
+
+    def zero_grad(self, set_to_none: bool = False) -> None:
+        r"""
+        Zero's the gradients for buffers/states along with the parameters.
+        See :meth:`torch.nn.Module.zero_grad` for details
+        """
+        # Zero grad parameters
+        super().zero_grad(set_to_none)
+        # Zero grad buffers
+        for b in self.buffers():
+            if b.grad_fn is not None:
+                b.detach_()
+            else:
+                b.requires_grad_(False)
+
+    def get_output_shape(self, in_shape: Tuple[int]) -> Tuple[int]:
+        """
+        Returns the output shape for passthrough implementation
+
+        Parameters
+        ----------
+        in_shape: Tuple of integers
+            Input shape
+
+        Returns
+        -------
+        Tuple of input shape
+            Output shape at given input shape
+        """
+        return in_shape
+
+    def reset_states(self, shape=None, randomize=False):
+        """
+        Reset the state of all neurons in this layer
+        """
+        device = self.state.device
+        if shape is None:
+            shape = self.state.shape
+
+        if randomize:
+            # State between lower and upper threshold
+            low = self.threshold_low or -self.threshold
+            width = self.threshold - low
+            self.state = torch.rand(shape, device=device) * width + low
+        else:
+            self.state = torch.zeros(shape, device=self.state.device)
+
+    def forward(self, input_current: torch.Tensor):
+        # Ensure the neuron state are initialized
+        shape_notime = (input_current.shape[0], *input_current.shape[2:])
+        if self.state.shape != shape_notime:
+            self.reset_states(shape=shape_notime, randomize=False)
+
+        # Determine no. of time steps from input
+        time_steps = input_current.shape[1]
+
+        state = self.state
+
+        alpha = torch.exp(- 1.0/self.tau_mem)
+
+        out_state = []
+
+        for iCurrentTimeStep in range(time_steps):
+            state = alpha * state  # leak state
+            state = state + input_current[:, iCurrentTimeStep]  # Add input
+            out_state.append(state)
+
+        self.state = state
+        self.tw = time_steps
+
+        out_state = torch.stack(out_state).transpose(0, 1)
+
+        return out_state
+
+
+ExpLeakSqueeze = squeeze_class(ExpLeak)
--- sinabs/layers/lif.py
+++ sinabs/layers/lif.py
@@ -1,12 +1,20 @@
 from typing import Optional, Union
-
+import torch
 from .spiking_layer import SpikingLayer
 from .pack_dims import squeeze_class
+from .functional import threshold_subtract, threshold_reset
+
+
+__all__ = ["LIF", "LIFSqueeze"]
+
+# Learning window for surrogate gradient
+window = 1.0
 
 
 class LIF(SpikingLayer):
     def __init__(
         self,
+        tau_mem: Union[float, torch.Tensor],
         threshold: float = 1.0,
         threshold_low: Union[float, None] = -1.0,
         membrane_subtract: Optional[float] = None,
@@ -37,9 +45,69 @@ class LIF(SpikingLayer):
             membrane_subtract=membrane_subtract,
             membrane_reset=membrane_reset,
         )
+        self.tau_mem = tau_mem
 
-    def forward(self, data):
-        raise NotImplementedError
+    def generate_spikes(self):
+        """
+        Given the parameters, compute the spikes that will be generated.
+        NOTE: This method only computes the spikes but does not reset the membrate potential.
+        """
+        # generate spikes
+        if self.membrane_reset:
+            self.activations = threshold_reset(self.state, self.threshold, self.threshold * window)
+        else:
+            self.activations = threshold_subtract(self.state, self.threshold, self.threshold * window)
+
+    def update_state_after_spike(self):
+        if self.membrane_reset:
+            # sum the previous state only where there were no spikes
+            self.state = self.state * (self.activations == 0.0)
+        else:
+            # subtract a number of membrane_subtract's as there are spikes
+            self.state = self.state - self.activations * self.membrane_subtract
+
+    def do_leak(self):
+        alpha = torch.exp(- 1.0/self.tau_mem)
+        return alpha * self.state
+
+    def add_input(self, input_data):
+        self.state = self.state + input_data # Add input
+
+    def forward(self, input_current: torch.Tensor):
+        # Ensure the neuron state are initialized
+        shape_notime = (input_current.shape[0], *input_current.shape[2:])
+        if self.state.shape != shape_notime:
+            self.reset_states(shape=shape_notime, randomize=False)
+
+        # Determine no. of time steps from input
+        time_steps = input_current.shape[1]
+
+        spikes = []
+
+        for iCurrentTimeStep in range(time_steps):
+            # generate spikes
+            self.generate_spikes()
+            spikes.append(self.activations)
+
+            # Reset membrane potential
+            self.update_state_after_spike()
+
+            # Do leak
+            self.do_leak()
+
+            # Add input
+            self.add_input(input_current[:, iCurrentTimeStep])  # Add input
+
+            # Membrane potential lower bound
+            if self.threshold_low is not None:
+                # This is equivalent to functional.threshold. non zero threshold is not supported for onnx
+                self.state = torch.nn.functional.relu(self.state - self.threshold_low) + self.threshold_low
+
+        self.tw = time_steps
+        all_spikes = torch.stack(spikes).transpose(0, 1)
+        self.spikes_number = all_spikes.abs().sum()
+
+        return all_spikes
 
 
 LIFSqueeze = squeeze_class(LIF)
