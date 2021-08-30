52331d4a808d275dbb13bd72b9086fd60e85521c
Gregor Lenz <gregor.lenz@synsense.ai>
2021-08-30 10:43:23 +0200

    add tests for LIF/ALIF current integration, membrane decay and threshold decay

--- tests/test_alif.py
+++ tests/test_alif.py
@@ -6,20 +6,45 @@ def test_alif():
     batch_size = 10
     time_steps = 100
     input_current = torch.rand(batch_size, time_steps, 2, 7, 7)
-    layer = ALIF(tau_mem=1, threshold=1, tau_thresh=10, delta_thresh=0.3)
+    layer = ALIF(tau_mem=1, threshold=1, tau_threshold=10, threshold_adaptation=0.3)
     spike_output = layer(input_current)
 
     assert input_current.shape == spike_output.shape
     assert torch.isnan(spike_output).sum() == 0
     assert spike_output.sum() > 0
 
-def test_lif_squeezed():
+def test_alif_squeezed():
     batch_size = 10
     time_steps = 100
     input_current = torch.rand(batch_size*time_steps, 2, 7, 7)
-    layer = ALIFSqueeze(tau_mem=1, threshold=1, tau_thresh=1., batch_size=batch_size)
+    layer = ALIFSqueeze(tau_mem=1, threshold=1, tau_threshold=1, batch_size=batch_size)
     spike_output = layer(input_current)
     
     assert input_current.shape == spike_output.shape
     assert torch.isnan(spike_output).sum() == 0
     assert spike_output.sum() > 0
+
+def test_alif_minimum_spike_threshold():
+    batch_size = 10
+    time_steps = 100
+    threshold = 10
+    input_current = torch.zeros(batch_size, time_steps, 2, 7, 7)
+    layer = ALIF(tau_mem=100, threshold=threshold, tau_threshold=100)
+    spike_output = layer(input_current)
+
+    assert (layer.threshold >= threshold).all(), "Spike thresholds should not drop below initital threshold."
+
+def test_alif_spike_threshold_decay():
+    batch_size = 10
+    time_steps = 100
+    threshold = 1
+    input_current = torch.ones(batch_size, time_steps, 2, 7, 7)
+    input_current[:,1:,:,:] = 0 # only inject current in the first time step
+    layer = ALIF(tau_mem=100, tau_threshold=time_steps, threshold=threshold, threshold_adaptation=1)
+    spike_output = layer(input_current)
+
+    assert (layer.threshold >= threshold).all()
+    # decay only starts after 2 time steps: current integration and adaption
+    threshold_decay = torch.exp(torch.tensor(-(time_steps-2)/time_steps))
+    # account for rounding errors with .isclose()
+    assert torch.isclose(layer.threshold-threshold, threshold_decay, atol=1e-08).all(), "Neuron spike thresholds do not seems to decay correctly."
--- tests/test_lif.py
+++ tests/test_lif.py
@@ -1,12 +1,13 @@
 import torch
 from sinabs.layers import LIF, LIFSqueeze
+import numpy as np
 
 
 def test_lif():
     batch_size = 10
     time_steps = 100
     input_current = torch.rand(batch_size, time_steps, 2, 7, 7)
-    layer = LIF(tau_mem=1., threshold=1.)
+    layer = LIF(tau_mem=1, threshold=1)
     spike_output = layer(input_current)
 
     assert input_current.shape == spike_output.shape
@@ -19,7 +20,32 @@ def test_lif_squeezed():
     input_current = torch.rand(batch_size*time_steps, 2, 7, 7)
     layer = LIFSqueeze(tau_mem=1, threshold=1, batch_size=batch_size)
     spike_output = layer(input_current)
-    
+
     assert input_current.shape == spike_output.shape
     assert torch.isnan(spike_output).sum() == 0
     assert spike_output.sum() > 0
+
+def test_lif_input_integration():
+    batch_size = 10
+    time_steps = 100
+    threshold = 10
+    input_current = torch.ones(batch_size, time_steps, 2, 7, 7) * threshold
+    input_current[:,1:,:,:] = 0 # only inject current in the first time step
+    layer = LIF(tau_mem=time_steps, threshold=threshold)
+    spike_output = layer(input_current)
+
+    assert spike_output.sum() == np.product(input_current.shape) / time_steps, "Every neuron should spike exactly once."
+    assert spike_output[:,1].sum() == spike_output.sum(), "First output time step should contain all the spikes."
+
+def test_lif_membrane_decay():
+    batch_size = 10
+    time_steps = 100
+    input_current = torch.ones(batch_size, time_steps, 2, 7, 7)
+    input_current[:,1:,:,:] = 0 # only inject current in the first time step
+    layer = LIF(tau_mem=time_steps, threshold=10)
+    spike_output = layer(input_current)
+
+    # first time step is not decayed
+    membrane_decay = torch.exp(torch.tensor(-(time_steps-1)/time_steps))
+    # account for rounding errors with .isclose()
+    assert torch.isclose(layer.state, membrane_decay, atol=1e-08).all(), "Neuron membrane potentials do not seems to decay correctly."
