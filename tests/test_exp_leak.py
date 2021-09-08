fcbf6870621a000b6e39065cb1580fb9260653e6
Gregor Lenz <gregor.lenz@synsense.ai>
2021-09-08 08:43:16 +0200

    update LeakyExp tests

--- tests/test_exp_leak.py
+++ tests/test_exp_leak.py
@@ -5,9 +5,8 @@ import pytest
 
 
 def test_leaky():
-    batch_size = 10
     time_steps = 100
-    input_current = torch.rand(batch_size, time_steps, 2, 7, 7)
+    input_current = torch.rand(time_steps, 2, 7, 7)
     layer = ExpLeak(tau_mem=torch.tensor(1))
     membrane_output = layer(input_current)
 
@@ -29,13 +28,15 @@ def test_leaky_squeezed():
 def test_leaky_membrane_decay():
     batch_size = 10
     time_steps = 100
-    input_current = torch.ones(batch_size, time_steps, 2, 7, 7)
-    input_current[:,1:,:,:] = 0 # only inject current in the first time step
+    input_current = torch.zeros(batch_size, time_steps, 2, 7, 7)
+    input_current[:,0] = 1 # only inject current in the first time step
     layer = ExpLeak(tau_mem=torch.tensor(time_steps))
     membrane_output = layer(input_current)
 
     # first time step is not decayed
     membrane_decay = torch.exp(torch.tensor(-(time_steps-1)/time_steps))
     # account for rounding errors with .isclose()
+    assert (membrane_output[:,0] == 1).all(), "Output for first time step is not correct."
+    assert (membrane_output[:,-1] == layer.state).all(), "Output of last time step does not correspond to last layer state."
     assert torch.isclose(layer.state, membrane_decay, atol=1e-08).all(), "Neuron membrane potentials do not seems to decay correctly."
 
