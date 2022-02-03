0727bafd2a1c5b52fe35ecf6660ba6c38db5ab4b
Felix Bauer <felix.bauer@aictx.ai>
2022-02-03 15:30:56 +0100

    Unit tests for copying uninitialized layers. Makes sure that #25 is resolved.

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -35,13 +35,44 @@ def test_deepcopy_iaf():
             assert layer_orig.num_timesteps == layer_copy.num_timesteps
 
 
+def test_deepcopy_iaf_uninitialized():
+    from sinabs.layers import IAF, IAFSqueeze
+
+    kwargs = dict(threshold_low=-0.4)
+
+    layer = IAF(**kwargs)
+    layer_squeeze_nts = IAFSqueeze(**kwargs, num_timesteps=10)
+    layer_squeeze_batch = IAFSqueeze(**kwargs, batch_size=10)
+
+    for layer_orig in (layer, layer_squeeze_batch, layer_squeeze_nts):
+        layer_copy = deepcopy(layer_orig)
+
+        for p0, p1 in zip(layer_orig.parameters(), layer_copy.parameters()):
+            assert (p0 == p1).all()
+            assert p0 is not p1
+        for b0, b1 in zip(layer_orig.buffers(), layer_copy.buffers()):
+            assert (b0 == b1).all()
+            assert b0 is not b1
+
+        assert (
+            layer_copy.activation_fn.spike_threshold
+            == layer_orig.activation_fn.spike_threshold
+        )
+        assert layer_copy.threshold_low == layer_orig.threshold_low
+        if hasattr(layer_orig, "batch_size"):
+            assert layer_orig.batch_size == layer_copy.batch_size
+        if hasattr(layer_orig, "num_timesteps"):
+            assert layer_orig.num_timesteps == layer_copy.num_timesteps
+
+
 def test_deepcopy_lif():
-    from sinabs.layers import LIF, LIFSqueeze, LIFRecurrent
+    from sinabs.layers import LIF, LIFSqueeze
 
     for train_alphas in (True, False):
-        kwargs = dict(tau_mem=torch.tensor(30.0), tau_syn=torch.tensor(10.0))
 
         input_current = torch.rand(10, 10, 10)
+        kwargs = dict(tau_mem=torch.tensor(30.0), tau_syn=torch.tensor(10.0))
+
         kwargs["train_alphas"] = train_alphas
 
         layer = LIF(**kwargs)
@@ -51,6 +82,48 @@ def test_deepcopy_lif():
 
         for layer_orig in (layer, layer_squeeze_batch, layer_squeeze_nts):
             layer_orig(input_current)
+
+            layer_copy = deepcopy(layer_orig)
+
+            for p0, p1 in zip(layer_orig.parameters(), layer_copy.parameters()):
+                assert (p0 == p1).all()
+                assert p0 is not p1
+            for b0, b1 in zip(layer_orig.buffers(), layer_copy.buffers()):
+                assert (b0 == b1).all()
+                assert b0 is not b1
+
+            if train_alphas:
+                assert layer_copy.alpha_mem == layer_orig.alpha_mem
+            else:
+                assert layer_copy.tau_mem == layer_orig.tau_mem
+            assert layer_copy.activation_fn == layer_orig.activation_fn
+            assert layer_copy.threshold_low == layer_orig.threshold_low
+            assert layer_copy.train_alphas == layer_orig.train_alphas
+            if hasattr(layer_orig, "batch_size"):
+                assert layer_orig.batch_size == layer_copy.batch_size
+            if hasattr(layer_orig, "num_timesteps"):
+                assert layer_orig.num_timesteps == layer_copy.num_timesteps
+            if hasattr(layer_orig, "rec_connect"):
+                assert (
+                    layer_orig.rec_connect.in_features
+                    == layer_copy.rec_connect.in_features
+                )
+
+
+def test_deepcopy_lif_uninitialized():
+    from sinabs.layers import LIF, LIFSqueeze
+
+    for train_alphas in (True, False):
+        kwargs = dict(tau_mem=torch.tensor(30.0), tau_syn=torch.tensor(10.0))
+
+        kwargs["train_alphas"] = train_alphas
+
+        layer = LIF(**kwargs)
+        layer_squeeze_batch = LIFSqueeze(**kwargs, batch_size=10)
+        layer_squeeze_nts = LIFSqueeze(**kwargs, num_timesteps=10)
+        #         layer_recurrent = LIFRecurrent(**kwargs, rec_connect=torch.nn.Linear(10,10))
+
+        for layer_orig in (layer, layer_squeeze_batch, layer_squeeze_nts):
 
             layer_copy = deepcopy(layer_orig)
 
