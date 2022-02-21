5c8348c659e7cedd2b0a672908965c50fffd69b0
Gregor Lenz <mail@lenzgregor.com>
2022-02-21 18:09:10 +0000

    remove input layer and regroup pooling and to_spike layers

--- sinabs/layers/__init__.py
+++ sinabs/layers/__init__.py
@@ -1,11 +1,10 @@
 from .crop2d import Cropping2dLayer
-from .maxpool2d import SpikingMaxPooling2dLayer
-from .input_layer import InputLayer
+from .pool2d import SpikingMaxPooling2dLayer
 from .quantize import QuantizeLayer
 from .neuromorphic_relu import NeuromorphicReLU
-from .sumpool2d import SumPool2d
-from .img_to_spk import Img2SpikeLayer
-from .sig_to_spk import Sig2SpikeLayer
+from .pool2d import SumPool2d
+from .to_spike import Img2SpikeLayer
+from .to_spike import Sig2SpikeLayer
 from .stateful_layer import StatefulLayer
 from .iaf import IAF, IAFRecurrent, IAFSqueeze
 from .lif import LIF, LIFRecurrent, LIFSqueeze
--- sinabs/layers/pool2d.py
+++ sinabs/layers/pool2d.py
@@ -86,3 +86,15 @@ class SpikingMaxPooling2dLayer(nn.Module):
             width + sum(self.padding[:2]), self.pool_size[1], self.strides[1]
         )
         return channels, height_out, width_out
+
+
+class SumPool2d(torch.nn.LPPool2d):
+    """
+    Non-spiking sumpooling layer to be used in analogue Torch models. It is identical to torch.nn.LPPool2d with p=1.
+
+    :param kernel_size: the size of the window
+    :param stride: the stride of the window. Default value is kernel_size
+    :param ceil_mode: when True, will use ceil instead of floor to compute the output shape
+    """
+    def __init__(self, kernel_size, stride=None, ceil_mode=False):
+        super().__init__(1, kernel_size, stride, ceil_mode)
