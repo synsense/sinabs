e566b0530ae84f252e999b21025e4cd83bbc0e4f
Yalun_Hu <yalun.hu@synsense.ai>
2022-06-13 13:50:55 +0800

    override get constraints method for speck2cmini device

--- sinabs/backend/dynapcnn/chips/speck2cmini.py
+++ sinabs/backend/dynapcnn/chips/speck2cmini.py
@@ -8,7 +8,10 @@ else:
     SAMNA_AVAILABLE = True
 from .dynapcnn import DynapcnnConfigBuilder
 
+from typing import List
 from sinabs.backend.dynapcnn.dynapcnn_layer import DynapcnnLayer
+from sinabs.backend.dynapcnn.mapping import LayerConstraints
+
 
 # Since most of the configuration is identical to DYNAP-CNN, we can simply inherit this class
 
@@ -24,7 +27,7 @@ class Speck2CMiniConfigBuilder(DynapcnnConfigBuilder):
 
     @classmethod
     def get_output_buffer(cls):
-        return samna.BufferSinkNode_speck2c_mini_event_output_event()
+        return samna.BasicSinkNode_speck2c_mini_event_output_event()
 
     @classmethod
     def get_dynapcnn_layer_config_dict(cls, layer: DynapcnnLayer):
@@ -33,3 +36,36 @@ class Speck2CMiniConfigBuilder(DynapcnnConfigBuilder):
         config_dict.pop("biases_kill_bit")
         config_dict.pop("neurons_value_kill_bit")
         return config_dict
+
+    @classmethod
+    def get_constraints(cls) -> List[LayerConstraints]:
+
+        weights_memory_size = [
+            16 * 1024,  # 0
+            32 * 1024,  # 1
+            32 * 1024,  # 2
+            64 * 1024,  # 3
+            16 * 1024,  # 4
+        ]
+
+        neurons_memory_size = [
+            64 * 1024,  # 0
+            32 * 1024,  # 1
+            32 * 1024,  # 2
+            16 * 1024,  # 3
+            16 * 1024,  # 4
+        ]
+
+        bias_memory_size = [
+            0,     # 0
+            0,     # 1
+            0,     # 2
+            1024,  # 3
+            1024   # 4
+        ]
+
+        constraints = [
+            LayerConstraints(km, nm, bm) for (km, nm, bm) in
+            zip(weights_memory_size, neurons_memory_size, bias_memory_size)
+        ]
+        return constraints
