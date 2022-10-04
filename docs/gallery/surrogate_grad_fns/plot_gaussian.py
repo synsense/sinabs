fix MultiGaussian surrogate gradient and add Gaussian surrogate gradient function

docs/gallery/surrogate_grad_fns/plot_gaussian.py
@@ -1,8 +1,8 @@
 """
-=============
-MultiGaussian
-=============
-:class:`~sinabs.activation.MultiGaussian` surrogate gradient.
+========
+Gaussian
+========
+:class:`~sinabs.activation.Gaussian` surrogate gradient.
 """
 
 import torch
@@ -10,7 +10,7 @@ import sinabs.activation as sina
 import matplotlib.pyplot as plt
 
 x = torch.linspace(-2, 4, 500)
-plt.plot(x, sina.MultiGaussian()(v_mem=x, spike_threshold=1), label="MultiGaussian")
+plt.plot(x, sina.Gaussian()(v_mem=x, spike_threshold=1.0), label="Gaussian")
 plt.xlabel("Neuron membrane potential")
 plt.ylabel("Derivative")
 plt.legend()

docs/gallery/surrogate_grad_fns/plot_periodicexponential.py
@@ -9,7 +9,7 @@ import torch
 import sinabs.activation as sina
 import matplotlib.pyplot as plt
 
-x = torch.linspace(-0.5, 3.5, 500)
+x = torch.linspace(-2, 4, 500)
 plt.plot(
     x,
     sina.PeriodicExponential()(v_mem=x, spike_threshold=1.0),

docs/gallery/surrogate_grad_fns/plot_singleexponential.py
@@ -9,10 +9,11 @@ import torch
 import sinabs.activation as sina
 import matplotlib.pyplot as plt
 
-x = torch.linspace(-0.5, 3.5, 500)
+x = torch.linspace(-2, 4, 500)
 plt.plot(
     x, sina.SingleExponential()(v_mem=x, spike_threshold=1.0), label="SingleExponential"
 )
 plt.xlabel("Neuron membrane potential")
 plt.ylabel("Derivative")
 plt.legend()
+plt.show()

sinabs/activation/surrogate_gradient_fn.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import math
+from random import gauss
 import torch
 
 
@@ -20,6 +21,29 @@ class Heaviside:
         return ((v_mem >= (spike_threshold - self.window)).float()) / spike_threshold
 
 
+def gaussian(x: torch.Tensor, mu: float, sigma: float):
+    return torch.exp(-((x - mu) ** 2) / (2 * sigma**2)) / (
+        sigma * torch.sqrt(2 * torch.tensor(math.pi))
+    )
+
+
+@dataclass
+class Gaussian:
+    """
+    Gaussian surrogate gradient function.
+    """
+
+    mu: float = 0.0
+    sigma: float = 0.5
+    grad_scale: float = 1.0
+
+    def __call__(self, v_mem, spike_threshold):
+        return (
+            gaussian(x=v_mem - spike_threshold, mu=self.mu, sigma=self.sigma)
+            * self.grad_scale
+        )
+
+
 @dataclass
 class MultiGaussian:
     """
@@ -30,15 +54,22 @@ class MultiGaussian:
 
     mu: float = 0.0
     sigma: float = 0.5
+    h: float = 0.15
+    s: float = 6
     grad_scale: float = 1.0
 
     def __call__(self, v_mem, spike_threshold):
         return (
-            torch.exp(
-                -(((v_mem - spike_threshold) - self.mu) ** 2) / (2 * self.sigma**2)
+            (1 + self.h)
+            * gaussian(x=v_mem - spike_threshold, mu=self.mu, sigma=self.sigma)
+            - self.h
+            * gaussian(
+                x=v_mem - spike_threshold, mu=self.sigma, sigma=self.s * self.sigma
             )
-            / torch.sqrt(2 * torch.tensor(math.pi))
-            / self.sigma
+            - self.h
+            * gaussian(
+                x=v_mem - spike_threshold, mu=-self.sigma, sigma=self.s * self.sigma
+            )
         ) * self.grad_scale
 
 