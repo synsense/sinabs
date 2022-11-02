833fc14bff53a46aca20111fbd4f399d114ce2da
Felix Bauer <felix.bauer@aictx.ai>
2022-11-02 13:23:37 +0100

    Enable top-level conversion of sinabs layers with 'replace_module' function. Resolves issue #60.

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,6 +1,7 @@
 import sinabs
 import sinabs.layers as sl
 import torch.nn as nn
+import pytest
 
 
 def test_layer_replacement_sequential():
@@ -46,3 +47,24 @@ def test_layer_replacement_arbitrary():
     assert filtered_model.spike1.spike_threshold == 2
     assert filtered_model.spike2.spike_threshold == 1
     assert model is not filtered_model
+
+
+def test_layer_replacement_individual():
+    layer = sl.IAF(spike_threshold=2.0)
+    mapper_fn = lambda module: sl.IAFSqueeze(**module.arg_dict, batch_size=4)
+    squeezed_layer = sinabs.conversion.replace_module(layer, sl.IAF, mapper_fn)
+    
+    assert type(squeezed_layer) == sl.IAFSqueeze
+    assert squeezed_layer.batch_size == 4
+    assert squeezed_layer.spike_threshold == 2
+
+
+def test_layer_replacement_individual_in_place():
+    layer = sl.IAF(spike_threshold=2.0)
+    mapper_fn = lambda module: sl.IAFSqueeze(**module.arg_dict, batch_size=4)
+    with pytest.warns(UserWarning):
+        squeezed_layer = sinabs.conversion.replace_module_(layer, sl.IAF, mapper_fn)
+    
+    assert type(layer) == sl.IAF
+    assert squeezed_layer is None
+
