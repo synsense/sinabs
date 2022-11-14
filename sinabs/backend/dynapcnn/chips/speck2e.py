316d07dae16cc3615fb1c951fe18a0427ceb7cb8
Sadique Sheik <sadique.sheik@synsense.ai>
2022-11-14 22:01:41 +0100

    moved kill bits to a separate method

--- sinabs/backend/dynapcnn/chips/speck2e.py
+++ sinabs/backend/dynapcnn/chips/speck2e.py
@@ -1,4 +1,5 @@
 from subprocess import CalledProcessError
+from sinabs.backend.dynapcnn.dynapcnn_layer import DynapcnnLayer
 try:
     import samna
     from samna.speck2e.configuration import SpeckConfiguration
@@ -24,3 +25,12 @@ class Speck2EConfigBuilder(DynapcnnConfigBuilder):
     @classmethod
     def get_output_buffer(cls):
         return samna.BasicSinkNode_speck2e_event_output_event()
+
+    @classmethod
+    def set_kill_bits(cls, layer: DynapcnnLayer, config_dict: dict) -> dict:
+        """This chip has no kill bits. So this function doesn't do anything
+
+        Returns:
+            dict: config_dict
+        """
+        return config_dict
