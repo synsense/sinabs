2f266b8171235bda9226cd57ffe8c3bb2db56b26
nogay.kuepelioglu <nogay.kuepelioglu@synsense.ai>
2023-01-26 16:24:20 +0100

    For non-squeeze layers in the case of input with different batch sizes are received, the states are going to be reshaped accordingly. The change will be randomly sampling states from created by samples in the existing batch repeatedly.

---
+++ tests/test_batch_mismatch.py
@@ -0,0 +1,52 @@
+def test_larger_batch_size_next():
+    import torch
+    import sinabs.layers as sl
+
+    layer = sl.IAF()
+
+    first_batch = torch.zeros(size=(2, 10, 1, 28, 28)) 
+    second_batch = torch.zeros(size=(4, 10, 1, 28, 28))
+
+    first_out = layer(first_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([first_batch.shape[0], *first_batch.shape[2:]])
+    assert first_out.shape[0] == first_batch.shape[0]
+    second_out = layer(second_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([second_batch.shape[0], *second_batch.shape[2:]])
+    assert second_out.shape[0] == second_batch.shape[0]
+
+def test_smaller_batch_size_next():
+    import torch
+    import sinabs.layers as sl
+
+    layer = sl.IAF()
+    first_batch = torch.ones(size=(4, 10, 1, 28, 28)) 
+    second_batch = torch.ones(size=(2, 10, 1, 28, 28))
+
+    first_out = layer(first_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([first_batch.shape[0], *first_batch.shape[2:]])
+    assert first_out.shape[0] == first_batch.shape[0]
+    second_out = layer(second_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([second_batch.shape[0], *second_batch.shape[2:]])
+    assert second_out.shape[0] == second_batch.shape[0]
+
+def test_same_batch_size_next():
+    import torch
+    import sinabs.layers as sl
+
+    layer = sl.IAF()
+
+    first_batch = torch.zeros(size=(2, 10, 1, 28, 28)) 
+    second_batch = torch.zeros(size=(2, 10, 1, 28, 28))
+
+    first_out = layer(first_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([first_batch.shape[0], *first_batch.shape[2:]])
+    assert first_out.shape[0] == first_batch.shape[0]
+    second_out = layer(second_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([second_batch.shape[0], *second_batch.shape[2:]])
+    assert second_out.shape[0] == second_batch.shape[0]
