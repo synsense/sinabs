e7965bfb00b25717407ca0fe710930df2b9af8e2
nogay.kuepelioglu <nogay.kuepelioglu@synsense.ai>
2023-01-26 16:49:29 +0100

    Add test for changing the trailing dim, which should not happen.

--- tests/test_batch_mismatch.py
+++ tests/test_batch_mismatch.py
@@ -50,3 +50,22 @@ def test_same_batch_size_next():
     for buff in layer.buffers():
         assert buff.size() == torch.Size([second_batch.shape[0], *second_batch.shape[2:]])
     assert second_out.shape[0] == second_batch.shape[0]
+
+def test_trailing_dim_change():
+    import torch
+    import sinabs.layers as sl
+
+    layer = sl.IAF()
+
+    first_batch = torch.zeros(size=(2, 10, 1, 28, 28)) 
+    second_batch = torch.zeros(size=(2, 10, 1, 26, 26))
+
+    first_out = layer(first_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([first_batch.shape[0], *first_batch.shape[2:]])
+    assert first_out.shape[0] == first_batch.shape[0]
+    second_out = layer(second_batch)
+    for buff in layer.buffers():
+        assert buff.size() == torch.Size([second_batch.shape[0], *second_batch.shape[2:]])
+    assert second_out.shape[0] == second_batch.shape[0]
+    
