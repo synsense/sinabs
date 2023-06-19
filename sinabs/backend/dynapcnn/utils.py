144f7a16ad9f0e7c09c1b12abd9d6dca504b6678
shynuie <yuxian.fan@synsense.ai>
2023-06-19 10:53:56 +0800

    modify the documentation for extend method

--- sinabs/backend/dynapcnn/utils.py
+++ sinabs/backend/dynapcnn/utils.py
@@ -496,48 +496,7 @@ def standardize_device_id(device_id: str) -> str:
 
 
 def extend_readout_layer(model: "DynapcnnNetwork") -> "DynapcnnNetwork":
-    """This function extends the Conv layer in readout layer of the model to 4 times the number of output channels.
-    For Speck 2E and 2F, to get the correct output, we need to extend the readout layer to 4 times the number of output.
-
-    Args:
-        model (DynapcnnNetwork): the model to be extended
-
-    Returns:
-        DynapcnnNetwork: the extended model
-    """
-    model = deepcopy(model)
-    input_shape = model.input_shape
-    og_readout_conv_layer = model.sequence[
-        -1
-    ].conv_layer  # extract the conv layer from dynapcnn network
-    og_weight_data = og_readout_conv_layer.weight.data
-    og_bias_data = og_readout_conv_layer.bias
-    og_bias = og_bias_data is not None
-    # modify the out channels
-    og_out_channels = og_readout_conv_layer.out_channels
-    new_out_channels = (og_out_channels - 1) * 4 + 1
-    og_readout_conv_layer.out_channels = new_out_channels
-    # build extended weight and replace the old one
-    ext_weight_shape = (new_out_channels, *og_weight_data.shape[1:])
-    ext_weight_data = torch.zeros(ext_weight_shape, dtype=og_weight_data.dtype)
-    for i in range(og_out_channels):
-        ext_weight_data[i * 4] = og_weight_data[i]
-    og_readout_conv_layer.weight.data = ext_weight_data
-    # build extended bias and replace if necessary
-    if og_bias:
-        ext_bias_data = torch.zeros(ext_bias_shape, dtype=og_bias_data.dtype)
-        ext_bias_shape = (new_out_channels,)
-        for i in range(og_out_channels):
-            ext_bias_data[i * 4] = og_bias_data[i]
-        og_readout_conv_layer.bias.data = ext_bias_data
-    _ = model(
-        torch.zeros(size=(1, *input_shape))
-    )  # run a forward pass to initialize the new weights and last IAF
-    return model
-
-
-def extend_readout_layer(model: "DynapcnnNetwork") -> "DynapcnnNetwork":
-    """This function extends the Conv layer in readout layer of the model to 4 times the number of output channels.
+    """Return a copied and extended model with the readout layer extended to 4 times the number of output channels.
     For Speck 2E and 2F, to get the correct output, we need to extend the readout layer to 4 times the number of output.
 
     Args:
