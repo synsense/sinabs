14bf2ac24a6255d5252094b275289ccda6325d43
shynuie <yuxian.fan@synsense.ai>
2023-06-19 10:44:03 +0800

    add readout layer extend for Speck2E and 2F

--- sinabs/backend/dynapcnn/chips/speck2e.py
+++ sinabs/backend/dynapcnn/chips/speck2e.py
@@ -1,7 +1,12 @@
+from typing import List
+
 import samna
+import torch
 from samna.speck2e.configuration import SpeckConfiguration
+from torch.nn import Conv2d
 
 from sinabs.backend.dynapcnn.dynapcnn_layer import DynapcnnLayer
+from sinabs.backend.dynapcnn.utils import extend_readout_layer
 
 from .dynapcnn import DynapcnnConfigBuilder
 
@@ -33,3 +38,13 @@ class Speck2EConfigBuilder(DynapcnnConfigBuilder):
     def get_dynapcnn_layer_config_dict(cls, layer: DynapcnnLayer):
         config_dict = super().get_dynapcnn_layer_config_dict(layer=layer)
         return config_dict
+
+    @classmethod
+    def build_config(cls, model: "DynapcnnNetwork", chip_layers: List[int]):
+        if model._is_chip_layers_ordering_auto:
+            print(f"Extend readout layer for Speck2E")
+            model = extend_readout_layer(
+                model
+            )  # extend only if chip layers ordering is determined automatically
+            chip_layers = cls.get_valid_mapping(model=model)
+        return super().build_config(model, chip_layers)
--- sinabs/backend/dynapcnn/chips/speck2f.py
+++ sinabs/backend/dynapcnn/chips/speck2f.py
@@ -1,7 +1,9 @@
 import samna
+from typing import List
 from samna.speck2f.configuration import SpeckConfiguration
 
 from sinabs.backend.dynapcnn.dynapcnn_layer import DynapcnnLayer
+from sinabs.backend.dynapcnn.utils import extend_readout_layer
 
 from .dynapcnn import DynapcnnConfigBuilder
 
@@ -32,3 +34,13 @@ class Speck2FConfigBuilder(DynapcnnConfigBuilder):
         config_dict.pop("biases_kill_bit")
         config_dict.pop("neurons_value_kill_bit")
         return config_dict
+
+    @classmethod
+    def build_config(cls, model: "DynapcnnNetwork", chip_layers: List[int]):
+        if model._is_chip_layers_ordering_auto:
+            print(f"Extend readout layer for Speck2E")
+            model = extend_readout_layer(
+                model
+            )  # extend only if chip layers ordering is determined automatically
+            chip_layers = cls.get_valid_mapping(model=model)
+        return super().build_config(model, chip_layers)
