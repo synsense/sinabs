919dd8012c1e757461d4c032e94f4881416c2d1e
Nogay Kuepelioglu <nogay.kuepelioglu@synsense.ai>
2023-07-02 06:41:23 +0200

    Add missing nodes

--- sinabs/nir.py
+++ sinabs/nir.py
@@ -1,13 +1,8 @@
-from typing import Callable, Optional, Tuple, Type
-from warnings import warn
+from typing import Optional, Union
 
 import torch
 from torch import nn
-
-import sinabs
 import sinabs.layers as sl
-from sinabs.activation import MembraneSubtract, MultiSpike, SingleExponential
-from sinabs.conversion import replace_module
 
 
 node_conversion_functions = {
@@ -18,33 +13,110 @@ node_conversion_functions = {
     nir.Conv2d: conv2d_from_nir,
 }
 
-def lif_from_nir(
-    unit: nir.LIF,
+def expleak_from_nir(
+    node: nir.LI,
     batch_size: Optional[int]=None,
     num_timesteps: Optional[int]=None
 ):
     if v_leak != 0:
         raise ValueError("`v_leak` must be 0")
 
-    parameters = dict(
-        tau_mem=torch.from_numpy(unit.tau),
-        min_v_mem=None,
-        num_timesteps=num_timesteps,
-        batch_size=batch_size,
-    )
-
-    if unit.alpha == unit.tau:
-        parameters["norm_input"] = False
-    elif unit.alpha == unit.tau - 1:
-        parameters["norm_input"] = True
+    if node.alpha == node.tau:
+        norm_input = False
+    elif node.alpha == node.tau - 1:
+        norm_input = True
     else:
         raise ValueError("`alpha` must be either `tau` or `tau-1`")
 
     return sl.LIFSqueeze(
-        **parameters,
-        spike_threshold=unit.threshold,
+        tau_mem=torch.from_numpy(node.tau),
+        min_v_mem=None,
+        num_timesteps=num_timesteps,
+        batch_size=batch_size,
         tau_syn=None,
+        norm_input=norm_input,
     )
+
+def lif_from_nir(
+    node: nir.LIF,
+    batch_size: Optional[int]=None,
+    num_timesteps: Optional[int]=None
+):
+    if v_leak != 0:
+        raise ValueError("`v_leak` must be 0")
+
+    if node.alpha == node.tau:
+        norm_input = False
+    elif node.alpha == node.tau - 1:
+        norm_input = True
+    else:
+        raise ValueError("`alpha` must be either `tau` or `tau-1`")
+
+    return sl.LIFSqueeze(
+        tau_mem=torch.from_numpy(node.tau),
+        min_v_mem=None,
+        num_timesteps=num_timesteps,
+        batch_size=batch_size,
+        spike_threshold=node.threshold,
+        tau_syn=None,
+        norm_input=norm_input,
+    )
+
+
+def linear_from_nir(
+    node: nir.LIF,
+    batch_size: Optional[int]=None,
+    num_timesteps: Optional[int]=None
+):
+    linear = nn.Linear(
+        in_features=node.weights.shape[1],
+        out_features=node.weights.shape[0],
+        bias=True,
+    )
+    linear.weight.data = torch.from_numpy(node.weights).float()
+    linear.bias.data = torch.from_numpy(node.bias).float()
+    return linear
+
+
+def conv1d_from_nir(
+    node: nir.Conv1d,
+    batch_size: Optional[int]=None,
+    num_timesteps: Optional[int]=None
+):
+    conv = nn.Conv1d(
+        in_channels=node.weights.shape[1],
+        out_channels=node.weights.shape[0],
+        kernel_size=node.weights.shape[2:],
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups,
+        bias=True,
+    )
+    conv.weight.data = torch.from_numpy(node.weights).float()
+    conv.bias.data = torch.from_numpy(node.bias).float()
+    return conv
+
+
+def conv2d_from_nir(
+    node: nir.Conv2d,
+    batch_size: Optional[int]=None,
+    num_timesteps: Optional[int]=None
+):
+    conv = nn.Conv2d(
+        in_channels=node.weights.shape[1],
+        out_channels=node.weights.shape[0],
+        kernel_size=node.weights.shape[2:],
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups,
+        bias=True,
+    )
+    conv.weight.data = torch.from_numpy(node.weights).float()
+    conv.bias.data = torch.from_numpy(node.bias).float()
+    return conv
+
 
 def from_nir(
     source: Union[Path, str, NIR], batch_size: int 
@@ -64,5 +136,5 @@ def from_nir(
         for node in source.nodes
     ]
 
-
+    #TODO: Map NIRTorch graph to torch module
     
