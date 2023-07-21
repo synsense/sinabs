8e1b12af973b514cb5a9b0fd3d677e6ce3d17bd5
nogay.kuepelioglu <nogay.kuepelioglu@synsense.ai>
2023-07-21 13:12:46 +0200

    Change the MNIST example to work with the new structure.

--- examples/mnist/dynapcnn_network.py
+++ examples/mnist/dynapcnn_network.py
@@ -1,15 +1,43 @@
 import time
-import torch
-import samna
+from pathlib import Path
+
 import numpy as np
+import torch
 import torch.nn as nn
 from torchvision import datasets
-from sinabs.from_torch import from_model
-from sinabs.backend.dynapcnn import io
-from sinabs.backend.dynapcnn import DynapcnnNetwork
 
+import samna
+from sinabs.from_torch import from_model
+from sinabs.backend.dynapcnn import DynapcnnNetwork
+from sinabs.backend.dynapcnn.io import close_device
 from sinabs.backend.dynapcnn.chip_factory import ChipFactory
 
+
+# Define the path to pre-trained MNIST weights.
+weights_path = Path( __file__ ).absolute().parent / "mnist_params.pt"
+
+# Define custom dataset for spiking input data
+input_shape = (1, 28, 28)
+class MNIST_Dataset(datasets.MNIST):
+    def __init__(self, root, train=True, spiking=False, t_window=100):
+        super().__init__(root, train=train, download=True)
+        self.spiking = spiking
+        self.t_window = t_window
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+
+        if self.spiking:
+            img = (np.random.rand(self.t_window, 1, *img.size()) < img.numpy() / 255.0).astype(float)
+            img = torch.from_numpy(img).float()
+        else:
+            # Convert image to tensor
+            img = torch.from_numpy(img.numpy()).float()
+            img.unsqueeze_(0)
+
+        return img, target
+
+# Define ANN
 ann = nn.Sequential(
     nn.Conv2d(1, 20, 5, 1, bias=False),
     nn.ReLU(),
@@ -26,81 +54,69 @@ ann = nn.Sequential(
     nn.Linear(500, 10, bias=False),
 )
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-ann = ann.to(device)
+device = "cpu"
+ann.to(device)
+ann.load_state_dict(torch.load(weights_path, map_location=device))
 
-ann.load_state_dict(torch.load("mnist_params.pt", map_location=device))
+# Scale weights
+with torch.no_grad():
+    ann[0].weight.data *= 0.5 
 
-sinabs_model = from_model(ann, add_spiking_output=True)
+# Convert SNN
+sinabs_model = from_model(ann, add_spiking_output=True, batch_size=1).spiking_model
 
-input_shape = (1, 28, 28)
-
+# Convert SNN to 
 hardware_compatible_model = DynapcnnNetwork(
-    sinabs_model.spiking_model.cpu(),
+    sinabs_model,
     discretize=True,
     input_shape=input_shape,
 )
 
+# Chip name
 # Depending on the available hardware you have, you can choose to run the
 # network for example on 'dynapcnndevkit' or 'speck2b' among others. See
 # documentation for full list of supported devices
+# chip_name = "speck2fmodule"
+chip_name = "dynapcnndevkit"
+
 hardware_compatible_model.to(
-    device="dynapcnndevkit:0",
-    monitor_layers=[-1]  # Last layer
+    device=chip_name,
+    monitor_layers=[-1] # Monitor the output layer 
 )
-
-
-# Define custom dataset for spiking input data
-class MNIST_Dataset(datasets.MNIST):
-
-    def __init__(self, root, train=True, spiking=False, tWindow=100):
-        super().__init__(root, train=train, download=True)
-        self.spiking = spiking
-        self.tWindow = tWindow
-
-    def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-
-        if self.spiking:
-            img = (np.random.rand(self.tWindow, 1, *img.size()) < img.numpy() / 255.0).astype(float)
-            img = torch.from_numpy(img).float()
-        else:
-            # Convert image to tensor
-            img = torch.from_numpy(img.numpy()).float()
-            img.unsqueeze_(0)
-
-        return img, target
-
 
 # Define dataloader
-tWindow = 200  # ms (or) time steps
+t_window = 100  # ms (or) time steps
 
-# Define test dataset loader
-test_dataset = MNIST_Dataset("./data", train=False, spiking=True, tWindow=tWindow)
+# Initialize the dataset and get the first sample and target 
+test_dataset = MNIST_Dataset("./data", train=False, spiking=True, t_window=t_window)
+first_sample = test_dataset[0][0]
+first_target = test_dataset[0][1]
+# Check the chip layer ordering to send the events to the correct core
 chip_layers_ordering = hardware_compatible_model.chip_layers_ordering
-print(f"The model was placed on the chip at the following layers: {chip_layers_ordering}")
-
-
-factory = ChipFactory("dynapcnndevkit:0")
-# Generate input events
+print(f"The model was placed on the chip at the following cores: {chip_layers_ordering}")
+# Convert the spike train raster to chip events 
+factory = ChipFactory(chip_name)
 input_events = factory.raster_to_events(
-    test_dataset[0][0],
+    first_sample,
     layer=chip_layers_ordering[0],  # First layer on the chip
+    dt=0.001
 )
-
-# Flush buffer
-hardware_compatible_model.samna_output_buffer.get_events()
 
 # Process events
 print("Sending events to device")
 evs_out = hardware_compatible_model(input_events)
-print(f"{len(evs_out)} output events read from time {evs_out[0].timestamp} to {evs_out[-1].timestamp}")
 
 # Filter readout events
-evs_out = list(filter(lambda x: isinstance(x, samna.dynapcnn.event.Spike), evs_out))
+if chip_name == "dynapcnndevkit":
+    evs_out = list(filter(lambda x: isinstance(x, samna.dynapcnn.event.Spike), evs_out))
 
-# Reading events out of the device
-for ev in evs_out:
-    print(ev.feature, ev.timestamp)
+if len(evs_out) > 0:
+    print(f"{len(evs_out)} output events read from time {evs_out[0].timestamp} to {evs_out[-1].timestamp}")
 
-io.close_device("dynapcnndevkit")
+    # Reading events out of the device
+    for ev in evs_out:
+        print(ev.feature, ev.timestamp)
+else:
+    print("No output events received!")
+
+close_device(chip_name)
