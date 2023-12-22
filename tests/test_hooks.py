396dbd5e0be00aec1931b0a069b901762465215a
Felix Bauer <felix.bauer@aictx.ai>
2023-12-22 17:44:59 +0100

    Unit tests for firing rate and input diff hooks

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,4 +1,5 @@
 from itertools import product
+from pathlib import Path
 
 import pytest
 import torch
@@ -8,6 +9,10 @@ import sinabs.layers as sl
 from sinabs import hooks
 from sinabs.layers import IAFSqueeze
 
+
+TEST_DIR = Path(__file__).resolve().parent
+INPUT_RESULT_DIR = TEST_DIR / "inputs_and_results" / "hooks"
+MODEL_DIR = TEST_DIR / "models"
 
 def test_linear_layer_synops_hook():
     model = nn.Linear(3, 5)
@@ -27,8 +32,8 @@ combinations = product(kernel_sizes, strides, paddings)
 
 @pytest.mark.parametrize("kernel_size,stride,padding", combinations)
 def test_conv_layer_synops_hook(kernel_size, stride, padding):
-    inp = torch.load("inputs_and_results/hooks/conv_input.pth")
-    correct_synops = torch.load("inputs_and_results/hooks/conv_layer_synops.pth")
+    inp = torch.load(INPUT_RESULT_DIR / "conv_input.pth")
+    correct_synops = torch.load(INPUT_RESULT_DIR / "conv_layer_synops.pth")
     model = nn.Conv2d(3, 5, kernel_size=kernel_size, stride=stride, padding=padding)
     model.register_forward_hook(hooks.conv_layer_synops_hook)
     model(inp)
@@ -37,9 +42,9 @@ def test_conv_layer_synops_hook(kernel_size, stride, padding):
 
 @pytest.mark.parametrize("dt", (None, 1, 0.1, 2))
 def test_model_synops_hook(dt):
-    inp = torch.load("inputs_and_results/hooks/conv_input.pth")
-    correct_synops = torch.load("inputs_and_results/hooks/model_synops.pth")
-    model = torch.load("models/synop_hook_model.pth")
+    inp = torch.load(INPUT_RESULT_DIR / "conv_input.pth")
+    correct_synops = torch.load(INPUT_RESULT_DIR / "model_synops.pth")
+    model = torch.load(MODEL_DIR / "synop_hook_model.pth")
     hooks.register_synops_hooks(model, dt=dt)
 
     model(inp)
@@ -48,3 +53,41 @@ def test_model_synops_hook(dt):
         if dt is not None:
             assert model[idx].synops_per_second == synops / dt
 
+def test_firing_rate_hook():
+    inp = torch.load(INPUT_RESULT_DIR / "conv_input.pth")
+    model = torch.load(MODEL_DIR / "synop_hook_model.pth")
+    correct_firing_rates = torch.load(
+        INPUT_RESULT_DIR / "firing_rates.pth"
+    )
+    for layer in model:
+        if isinstance(layer, IAFSqueeze):
+            layer.register_forward_hook(hooks.firing_rate_hook)
+    model(inp)
+    for idx, firing_rate in correct_firing_rates.items():
+        assert model[idx].firing_rate == firing_rate
+
+def test_firing_rate_per_neuron_hook():
+    inp = torch.load(INPUT_RESULT_DIR / "conv_input.pth")
+    model = torch.load(MODEL_DIR / "synop_hook_model.pth")
+    correct_firing_rates = torch.load(
+        INPUT_RESULT_DIR / "firing_rates_per_neuron.pth"
+    )
+    for layer in model:
+        if isinstance(layer, IAFSqueeze):
+            layer.register_forward_hook(hooks.firing_rate_per_neuron_hook)
+    model(inp)
+    for idx, firing_rate in correct_firing_rates.items():
+        assert (model[idx].firing_rate_per_neuron == firing_rate).all()
+
+def test_input_diff_hook():
+    inp = torch.load(INPUT_RESULT_DIR / "conv_input.pth")
+    model = torch.load(MODEL_DIR / "synop_hook_model.pth")
+    correct_values = torch.load(
+        INPUT_RESULT_DIR / "input_diffs.pth"
+    )
+    for layer in model:
+        if isinstance(layer, (nn.Conv2d, nn.Linear)):
+            layer.register_forward_hook(hooks.input_diff_hook)
+    model(inp)
+    for idx, correct in correct_values.items():
+        assert (model[idx].diff_output == correct).all()
