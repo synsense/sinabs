5091c657ece29aa0d9628c976ee42662d82de16e
Sadique Sheik <sadique.sheik@synsense.ai>
2024-01-30 15:33:32 +0100

    tests updated to new API

--- tests/test_dynapcnn/test_discover_device.py
+++ tests/test_dynapcnn/test_discover_device.py
@@ -1,32 +1,21 @@
 import pytest
+import samna
 
 from sinabs.backend.dynapcnn import io
 
 
 @pytest.mark.skip("Not suitable for automated testing. Depends on available devices")
 def test_list_all_devices():
-    devices = io.get_all_samna_devices()
-    print(devices)
-    for d in devices:
-        print(d.to_json())
-        print(d.device_type_name)
+    device_map = io.get_device_map()
+    # Ideally the device map needs to be tested against something expected.
+    raise NotImplementedError()
 
 
 @pytest.mark.skip("Not suitable for automated testing. Depends on available devices")
 def test_is_device_type():
-    devices = io.get_all_samna_devices()
+    devices = samna.device.get_all_devices()
     print([io.is_device_type(d, "dynapcnndevkit") for d in devices])
     print([io.is_device_type(d, "dvxplorer") for d in devices])
-
-
-@pytest.mark.skip("Not suitable for automated testing. Depends on available devices")
-def test_discover_device():
-    device = io.open_device("dvxplorer:0")
-    device = io.open_device("dvxplorer:0")
-    device = io.open_device("dynapcnndevkit:0")
-    io.close_device("dvxplorer:0")
-    io.close_device("dynapcnndevkit:0")
-    assert io.get_all_open_samna_devices() == []
 
 
 @pytest.mark.skip("Not suitable for automated testing. Depends on available devices")
