55d7202804eefd141b719670318655e33eb4ab02
Willian-Girao <williansoaresgirao@gmail.com>
2024-04-19 12:45:39 +0200

    mapping nodes I/O shapes

--- sinabs/backend/dynapcnn/NIRGraphExtractor.py
+++ sinabs/backend/dynapcnn/NIRGraphExtractor.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import nirtorch
 import copy
+import sinabs
+from typing import Tuple, Dict
 
 class NIRtoDynapcnnNetworkGraph():
     def __init__(self, spiking_model, dummy_input) -> None:
@@ -13,7 +15,7 @@ class NIRtoDynapcnnNetworkGraph():
         
         self.modules_map = self.get_named_modules(spiking_model)
 
-        # self.get_modules_io()
+        self.nodes_io_shapes = self.get_nodes_io_shapes(dummy_input)
 
     def get_edges_from_nir(self, nir_graph):
         """ ."""
@@ -53,36 +55,126 @@ class NIRtoDynapcnnNetworkGraph():
 
         return modules_map
     
-    def get_modules_io(self, input_dummy):
+    def get_nodes_io_shapes(self, input_dummy) -> Dict[int, Dict[str, torch.Size]]:
         """ ."""
-        modules_io_map = {}
+        nodes_io_map = {}
+        flagged_merge_nodes = {}
 
         for edge in self.edges_list:
             src = edge[0]
             trg = edge[1]
 
-            # pass input through source.
-            if src not in modules_io_map:
-                modules_io_map[src] = {'input': None, 'output': None}
+            if isinstance(self.modules_map[src], sinabs.layers.merge.Merge):
+                # At this point the output of Merge has to have been calculated.
+                
+                # pass input through target.
+                if trg not in nodes_io_map:
+                    nodes_io_map[trg] = {'input': None, 'output': None}
 
-            if src == 0:
-                _input = input_dummy
+                    inp_node = self.find_input_to_node(trg)                     # find node generating the input to be used.
+                    _input = nodes_io_map[inp_node]['output']
+
+                    _output = self.modules_map[trg](_input)                     # forward input through the node.
+
+                    nodes_io_map[trg] = {'input': _input, 'output': _output}  # save node's input/output.
+
+            elif isinstance(self.modules_map[trg], sinabs.layers.merge.Merge):
+                # Merge requires two inputs: need to check if both of its inputs have been calculated.
+                if trg not in flagged_merge_nodes:
+                    flagged_merge_nodes[trg] = {}
+                
+                args = self.find_merge_arguments(trg)
+
+                for arg in args:
+                    if arg in nodes_io_map:                          # one input to Merge has been computed.
+                        flagged_merge_nodes[trg][arg] = nodes_io_map[arg]
+
+                if len(flagged_merge_nodes[trg]) == 2:                 # both arguments to Merge have been computed.
+                    if trg not in nodes_io_map:
+                        nodes_io_map[trg] = {'input': None, 'output': None}
+
+                        _output = self.modules_map[trg](
+                            nodes_io_map[args[0]]['output'], 
+                            nodes_io_map[args[1]]['output'])
+                        
+                        _input = torch.max(torch.stack([                # Merge expands each input dim. into the max of that dim. between input tensors.
+                            nodes_io_map[args[0]]['output'], 
+                            nodes_io_map[args[1]]['output']]), dim=0)
+
+                        nodes_io_map[trg]['input'] = _input.values
+                        nodes_io_map[trg]['output'] = _output
+
+                # pass input through source.
+                if src not in nodes_io_map:
+                    nodes_io_map[src] = {'input': None, 'output': None}
+
+                    if src == 0:
+                        _input = input_dummy                                    # first node in the graph.
+                    else:
+                        inp_node = self.find_input_to_node(src)                 # find node generating the input to be used.
+                        _input = nodes_io_map[inp_node]['output']
+                    
+                    _output = self.modules_map[src](_input)                     # forward input through the node.
+
+                    nodes_io_map[src] = {'input': _input, 'output': _output}  # save node's input/output.
+
             else:
-                inp_node = self.find_my_input_node(src)
-                _input = modules_io_map[inp_node]['output']
-            
-            _output = self.modules_map[src](_input)
 
-            # pass input through target.
-            if trg not in modules_io_map:
-                modules_io_map[trg] = {'input': None, 'output': None}
+                # pass input through source.
+                if src not in nodes_io_map:
+                    nodes_io_map[src] = {'input': None, 'output': None}
 
-    def find_my_input_node(self, node_idx):
+                    if src == 0:
+                        _input = input_dummy                                    # first node in the graph.
+                    else:
+                        inp_node = self.find_input_to_node(src)                 # find node generating the input to be used.
+                        _input = nodes_io_map[inp_node]['output']
+                    
+                    _output = self.modules_map[src](_input)                     # forward input through the node.
+
+                    nodes_io_map[src] = {'input': _input, 'output': _output}  # save node's input/output.
+
+                # pass input through target.
+                if trg not in nodes_io_map:
+                    nodes_io_map[trg] = {'input': None, 'output': None}
+
+                    inp_node = self.find_input_to_node(trg)                     # find node generating the input to be used.
+                    _input = nodes_io_map[inp_node]['output']
+
+                    _output = self.modules_map[trg](_input)                     # forward input through the node.
+
+                    nodes_io_map[trg] = {'input': _input, 'output': _output}  # save node's input/output.
+
+        for node, io in nodes_io_map.items():
+            nodes_io_map[node]['input'] = io['input'].shape
+            nodes_io_map[node]['output'] = io['output'].shape
+
+        return nodes_io_map
+
+
+    def find_input_to_node(self, node):
+        """ ."""
         for edge in self.edges_list:
-            if edge[1] == node_idx:
+            if edge[1] == node:
                 return edge[0]
         return -1
+    
+    def find_node_variable_name(self, node):
+        """ ."""
+        for key, val in self.name_2_indx_map.items():
+            if val == node:
+                return key
+        return None
 
+    def find_merge_arguments(self, merge_node):
+        """ ."""
+        args = []
+        for edge in self.edges_list:
+            if edge[1] == merge_node:
+                args.append(edge[0])
+            if len(args) == 2:
+                break
+        return args
     
     def remove_ignored_nodes(self, default_ignored_nodes):
         """ Recreates the edges list based on layers that 'DynapcnnNetwork' will ignore. This
@@ -133,3 +225,7 @@ class NIRtoDynapcnnNetworkGraph():
             remapped_edges.append((remapped_nodes[edge[0]], remapped_nodes[edge[1]]))
 
         return remapped_edges, remapped_nodes
+    
+    def get_node_io_shapes(self, node) -> Tuple[torch.Size, torch.Size]:
+        """ ."""
+        return self.nodes_io_shapes[node]['input'], self.nodes_io_shapes[node]['output']
