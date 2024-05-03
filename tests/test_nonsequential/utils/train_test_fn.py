40b7091b231f98207b7ad293c9920e42db5da2ee
Willian-Girao <williansoaresgirao@gmail.com>
2024-05-03 23:45:38 +0200

    helper functions for HPO scripts

--- tests/test_nonsequential/utils/train_test_fn.py
+++ tests/test_nonsequential/utils/train_test_fn.py
@@ -56,6 +56,64 @@ def training_loop(device, nb_time_steps, batch_size, feature_map_size, dataloade
 
     return epochs_x, epochs_y, epochs_acc
 
+def training_loop_no_tqdm(device, nb_time_steps, batch_size, feature_map_size, dataloader_train, model, loss_fn, optimizer, epochs, dataloader_test):
+    model.train()
+
+    for e in range(epochs):
+        for X, y in dataloader_train:
+            # reshape the input from [Batch, Time, Channel, Height, Width] into [Batch*Time, Channel, Height, Width]
+            X = X.reshape(-1, feature_map_size[2], feature_map_size[0], feature_map_size[1]).to(dtype=torch.float, device=device)
+            y = y.to(dtype=torch.long, device=device)
+
+            # forward
+            pred = model(X)
+
+            # reshape the output from [Batch*Time,num_classes] into [Batch, Time, num_classes]
+            pred = pred.reshape(batch_size, nb_time_steps, -1)
+
+            # accumulate all time-steps output for final prediction
+            pred = pred.sum(dim = 1)
+            loss = loss_fn(pred, y)
+
+            # gradient update
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # detach the neuron states and activations from current computation graph(necessary)
+            model.detach_neuron_states()
+
+        acc = test_no_tqdm(device, nb_time_steps, batch_size, feature_map_size, dataloader_test, model)
+
+    return acc
+
+def test_no_tqdm(device, nb_time_steps, batch_size, feature_map_size, dataloader_test, model):
+    correct_predictions = []
+
+    with torch.no_grad():
+        for X, y in dataloader_test:
+            # reshape the input from [Batch, Time, Channel, Height, Width] into [Batch*Time, Channel, Height, Width]
+            X = X.reshape(-1, feature_map_size[2], feature_map_size[0], feature_map_size[1]).to(dtype=torch.float, device=device)
+            y = y.to(dtype=torch.long, device=device)
+
+            # forward
+            output = model(X)
+
+            # reshape the output from [Batch*Time,num_classes] into [Batch, Time, num_classes]
+            output = output.reshape(batch_size, nb_time_steps, -1)
+
+            # accumulate all time-steps output for final prediction
+            output = output.sum(dim=1)
+
+            # calculate accuracy
+            pred = output.argmax(dim=1, keepdim=True)
+
+            # compute the total correct predictions
+            correct_predictions.append(pred.eq(y.view_as(pred)))
+    
+    correct_predictions = torch.cat(correct_predictions)
+    return correct_predictions.sum().item()/(len(correct_predictions))*100
+
 def test(device, nb_time_steps, batch_size, feature_map_size, dataloader_test, model):
     correct_predictions = []
     with torch.no_grad():
