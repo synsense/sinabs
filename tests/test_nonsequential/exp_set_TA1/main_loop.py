a33ca0dc26d0a4b1182c5e51d33ef6f816a8c7be
Willian-Girao <williansoaresgirao@gmail.com>
2024-05-31 16:51:00 +0200

    removed folder with algo. exploration simulations

--- tests/test_nonsequential/exp_set_TA1/main_loop.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for w_load in range(3, 9):
-    print(w_load)
-    os.system(f'python train_script.py {w_load}')
--- tests/test_nonsequential/using_SumPool2d/ARCHITECTURES_SEARCH/train_all.py
+++
@@ -1,4 +0,0 @@
-import os
-
-for i in range(1, 9):
-    os.system(f'python model_training.py ResSCNN{i}')
