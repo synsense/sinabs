90f9575e04a8de129aeac4605c7b25e4d8adc868
Felix Bauer <felix.bauer@synsense.ai>
2024-10-25 17:01:48 +0200

    Integrate tests for sequential models in test_dynapcnnnetwork

--- tests/test_dynapcnnnetwork/conftest_dynapcnnnetwork.py
+++ tests/test_dynapcnnnetwork/conftest_dynapcnnnetwork.py
@@ -17,10 +17,14 @@ from .model_dummy_4 import batch_size as batch_size_4
 from .model_dummy_4 import expected_output as expected_output_4
 from .model_dummy_4 import input_shape as input_shape_4
 from .model_dummy_4 import snn as snn_4
+from .model_dummy_4 import snn as snn_4
+from .model_dummy_seq import input_shape_seq, seq_1, seq_2, expected_seq_1, expected_seq_2
 
 args_DynapcnnNetworkTest = [
     (snn_1, input_shape_1, batch_size_1, expected_output_1),
     (snn_2, input_shape_2, batch_size_2, expected_output_2),
     (snn_3, input_shape_3, batch_size_3, expected_output_3),
     (snn_4, input_shape_4, batch_size_4, expected_output_4),
+    (seq_1, input_shape_seq, 1, expected_seq_1),
+    (seq_2, input_shape_seq, 1, expected_seq_2),
 ]
