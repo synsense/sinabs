d7f6be31ae3c2842074ebc9732f44f8e44275553
Willian-Girao <williansoaresgirao@gmail.com>
2024-10-29 11:33:39 +0100

    WIP DVS - DVS node not given
    giving access to DVSLayer

--- sinabs/backend/dynapcnn/__init__.py
+++ sinabs/backend/dynapcnn/__init__.py
@@ -2,3 +2,4 @@ from .dynapcnn_layer import DynapcnnLayer
 from .dynapcnn_network import DynapcnnCompatibleNetwork, DynapcnnNetwork
 from .dynapcnnnetwork_module import DynapcnnNetworkModule
 from .dynapcnn_visualizer import DynapcnnVisualizer
+from .dvs_layer import DVSLayer
