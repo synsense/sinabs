57333cedbdcb4c06cb86c33ab4c0d69c8a2ac994
Felix Bauer <felix.bauer@synsense.ai>
2024-11-12 16:06:13 +0100

    Remove duplicate test

--- tests/test_dynapcnn/test_discover_device.py
+++ tests/test_dynapcnn/test_discover_device.py
@@ -4,14 +4,7 @@ import samna
 from sinabs.backend.dynapcnn import io
 
 
-@pytest.mark.skip("Not suitable for automated testing. Depends on available devices")
-def test_list_all_devices():
-    device_map = io.get_device_map()
-    # Ideally the device map needs to be tested against something expected.
-    raise NotImplementedError()
-
-
-@pytest.mark.skip("Not suitable for automated testing. Depends on available devices")
+pytest.mark.skip("Not suitable for automated testing. Depends on available devices")
 def test_is_device_type():
     devices = samna.device.get_all_devices()
     print([io.is_device_type(d, "dynapcnndevkit") for d in devices])
