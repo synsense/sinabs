d1fd3be411b0def7e54b6cbd049659367881766c
Phani Pavan K <kphanipavan@gmail.com>
2025-06-18 12:36:17 +0530

    brute force fix plots, take 4

--- docs/gallery/layers/plot_lif.py
+++ docs/gallery/layers/plot_lif.py
@@ -6,9 +6,28 @@ The :class:`~sinabs.layers.LIF` layer. This neuron integrates the input and deca
 """
 
 import torch
-from utils import plot_evolution
-
+import matplotlib.pytlot as plt
 import sinabs.layers as sl
+
+
+def plot_evolution(neuron_model, input: torch.Tensor):
+    neuron_model.reset_states()
+    output = neuron_model(input)
+
+    plt.figure(figsize=(10, 3))
+    for key, recording in neuron_model.recordings.items():
+        plt.plot(recording.detach().flatten(), drawstyle="steps", label=key)
+    plt.plot(
+        output.detach().flatten(), drawstyle="steps", color="black", label="output"
+    )
+    if "spike_threshold" not in neuron_model.recordings:
+        plt.plot(
+            [neuron_model.spike_threshold] * input.shape[1], label="spike_threshold"
+        )
+    plt.xlabel("time")
+    plt.title(f"{neuron_model.__class__.__name__} neuron dynamics")
+    plt.legend()
+
 
 const_current = torch.ones((1, 100, 1)) * 0.03
 single_current = torch.zeros((1, 100, 1))
