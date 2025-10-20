b4d92bfd7512c6b1e3b1ebcfd571e5f2b09ef0ab
Vanessa Leite <vrcleite@gmail.com>
2025-10-20 10:32:12 +0200

    Add missing speck2fdevkit as a supported device for tests

--- tests/test_dynapcnn/hw_utils.py
+++ tests/test_dynapcnn/hw_utils.py
@@ -32,6 +32,7 @@ supported_device_types_for_testing = {
     "speck2e": "Speck2eTestBoard",
     "speck2edevkit": "Speck2eDevKit",
     "speck2fmodule": "Speck2fModuleDevKit",
+    "speck2fdevkit": "Speck2fDevKit",
 }
 
 
